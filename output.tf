5bb07f44718921f5266a19e17bf2e9a204796896
sneharaut993 <shaharesp@gmail.com>
2025-09-16 11:25:45 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,5 +1,5 @@
 output "subnet_data" {
-  value = data.aws_subnet_ids.available_db_subnet.ids
+  value = data.aws_subnets.available_db_subnet.ids
 }
 output "rds_address" {
   value = aws_db_instance.db_instance.address
